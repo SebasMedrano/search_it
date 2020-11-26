20d941d0cf2395572c30fc05a4a9e599e454b4ed
Sebastian Medrano <sebastian.medrano@fundacion-jala.org>
2020-11-25 23:30:51 -0400

    routes updated

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -7,7 +7,7 @@ import { Producto } from '../Models/producto';
 const httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json',
                                                'Access-Control-Allow-Origin':'*',
                                                'Access-Control-Allow-Methods': 'POST'})};
-
+const base_route = "https://search-it-api.herokuapp.com"
 
 @Injectable({
   providedIn: 'root'
@@ -21,29 +21,29 @@ export class ProductoService {
   // }
 
   getProductsByComputacion(){
-    return this.http.get<any[]>('http://127.0.0.1:5000/products/computacion')
+    return this.http.get<any[]>(base_route + '/products/computacion')
   }
 
   getProductsByElectrodomestico(){
-    return this.http.get<any[]>('http://127.0.0.1:5000/products/electrodomestico')
+    return this.http.get<any[]>(base_route + '/products/electrodomestico')
   }
 
   getProductsByElectronico(){
-    return this.http.get<any[]>('http://127.0.0.1:5000/products/electronicos')
+    return this.http.get<any[]>(base_route + '/products/electronicos')
   }
 
   addProduct(product: Producto){
-    let postProduct = 'http://127.0.0.1:5000/products/'
+    let postProduct = base_route + '/products/'
     return this.http.post<Producto>(postProduct, product, httpOptions)
   }
 
   getProductsAll(){
-    return this.http.get<any[]>("http://127.0.0.1:5000/products/")
+    return this.http.get<any[]>(base_route + "/products/")
   }
 
   getProductSearched(buscado:string){
     console.log(buscado)
-    return this.http.get<any[]>('http://127.0.0.1:5000/productsOrdered/'.concat(buscado)) 
+    return this.http.get<any[]>(base_route + '/productsOrdered/'.concat(buscado)) 
   }
 
   // http://127.0.0.1:5000/products
